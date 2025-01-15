d408c5a839c246db39518e0095687bdc3564d9ca
mesachinjakhar <mesachinjakhar@gmail.com>
2025-01-15 19:12:06 +0530

    root loading in center of screen

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="flex items-center justify-center h-[85vh]">
+      <div className="logo flex gap-3 text-center">
+        <div className="w-16 md:w-20 lg:w-16">
+          <Image
+            src="/images/ignou_backbenchers_logo.webp"
+            alt="IGNOU Backbenchers Logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+          />
+        </div>
+        <Link href="/" aria-label="Go to Ignou BackBenchers Home">
+          <h2 className="text-2xl text-center text-black xs:text-lg">
+            Ignou BackBenchers
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
