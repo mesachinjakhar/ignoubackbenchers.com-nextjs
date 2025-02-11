16c4cfe96be5e0e91f6798c57aa6fa110929e056
Sachin Jakhar <mesachinjakhar@gmail.com>
2025-02-11 16:44:16 +0530

    css added for mobile

--- app/order/ignou-projects/page.jsx
+++ app/order/ignou-projects/page.jsx
@@ -1,10 +1,14 @@
 const Projects = () => {
   return (
-    <div className="px-44 mt-10">
-      <h1>💾 Download IGNOU Project and IGNOU Synopsis instantly</h1>
+    <div className="px-44 mt-10 xs:px-5">
+      <h1 className="xs:text-xl">
+        💾 Download IGNOU Project and IGNOU Synopsis instantly
+      </h1>
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">IGNOU MBA PROJECT</h1>
-        <p className="text-center text-lg">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
+          IGNOU MBA PROJECT
+        </h1>
+        <p className="text-center text-lg xs:text-sm">
           Download PDF file of IGNOU MBA Project & IGNOU MBA Synopsis Instantly
           i.e., MS 100 & MMPP 1.
         </p>
@@ -15,10 +19,10 @@ const Projects = () => {
       </div>
 
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
           IGNOU MA Psychology PROJECT
         </h1>
-        <p className="text-center text-lg">
+        <p className="text-center text-lg xs:text-sm">
           Download PDF file of IGNOU MAPC Project & IGNOU MAPC Synopsis
           Instantly i.e., MPCE 16, MPCE 26, & MPCE 36.
         </p>
@@ -29,8 +33,10 @@ const Projects = () => {
       </div>
 
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">IGNOU BTS PROJECT</h1>
-        <p className="text-center text-lg">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
+          IGNOU BTS PROJECT
+        </h1>
+        <p className="text-center text-lg xs:text-sm">
           Download PDF file of IGNOU BTS Project & IGNOU BTS Synopsis Instantly
           i.e., PTS 4, PTS 5, PTS 6.
         </p>
@@ -41,8 +47,10 @@ const Projects = () => {
       </div>
 
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">IGNOU AHE PROJECT</h1>
-        <p className="text-center text-lg">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
+          IGNOU AHE PROJECT
+        </h1>
+        <p className="text-center text-lg xs:text-sm">
           Download PDF file of IGNOU AHE Project & IGNOU AHE Synopsis Instantly
           i.e., AHE 01.
         </p>
@@ -53,8 +61,10 @@ const Projects = () => {
       </div>
 
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">IGNOU DNHE PROJECT</h1>
-        <p className="text-center text-lg">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
+          IGNOU DNHE PROJECT
+        </h1>
+        <p className="text-center text-lg xs:text-sm">
           Download PDF file of IGNOU DNHE Project & IGNOU DNHE Synopsis
           Instantly i.e., DNHE 4.
         </p>
@@ -65,8 +75,10 @@ const Projects = () => {
       </div>
 
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">IGNOU MTTM PROJECT</h1>
-        <p className="text-center text-lg">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
+          IGNOU MTTM PROJECT
+        </h1>
+        <p className="text-center text-lg xs:text-sm">
           Download PDF file of IGNOU MTTM Project & IGNOU MTTM Synopsis
           Instantly i.e., MTTM 16.
         </p>
@@ -77,10 +89,10 @@ const Projects = () => {
       </div>
 
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
           IGNOU MA Education PROJECT
         </h1>
-        <p className="text-center text-lg">
+        <p className="text-center text-lg xs:text-sm">
           Download PDF file of IGNOU MAEDU Project & IGNOU MAEDU Synopsis
           Instantly i.e., MESP 1.
         </p>
@@ -91,8 +103,10 @@ const Projects = () => {
       </div>
 
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">IGNOU MARD PROJECT</h1>
-        <p className="text-center text-lg">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
+          IGNOU MARD PROJECT
+        </h1>
+        <p className="text-center text-lg xs:text-sm">
           Download PDF file of IGNOU MARD Project & IGNOU MARD Synopsis
           Instantly i.e., MRDP 1.
         </p>
@@ -103,10 +117,10 @@ const Projects = () => {
       </div>
 
       <div className="w-[100%] mx-auto bg-teal-950 p-7 text-white mb-10">
-        <h1 className="text-center text-white mb-5">
+        <h1 className="text-center text-white mb-5 xs:text-xl">
           CUSTOMIZED IGNOU PROJECT OF ANY COURSE
         </h1>
-        <p className="text-center text-lg">
+        <p className="text-center text-lg xs:text-sm">
           We are here to assist you with your IGNOU Project work. You may order
           a customised project here; either you provide a topic or we will
           create one for you based on your synopsis, and if your synopsis is
